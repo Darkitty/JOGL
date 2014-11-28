27b29e9e673b27bc911a9dfaf77754d142641ebd
Nicolas Le Gall <nicolaslegall34@gmail.com>
2014-11-28 12:02:21 +0100

    Reindent all code

--- src/Main.java
+++ src/Main.java
@@ -11,193 +11,192 @@ import com.jogamp.opengl.util.FPSAnimator;
 
 @SuppressWarnings("serial")
 public class Main extends Frame{
-        // Composant awt, pour le rendu OpenGL (JPanel)
-        private GLCanvas canvas;                      
-        
-        // Gestionnaire du FPS, permet de ménager le CPU
-        private final FPSAnimator animator;    
+		// Composant awt, pour le rendu OpenGL (JPanel)
+	private GLCanvas canvas;                      
 
-        
-        // Classe interne chargée des évenements claviers
-        private static final class MyKeyListener extends KeyAdapter {
+		// Gestionnaire du FPS, permet de ménager le CPU
+	private final FPSAnimator animator;    
 
-                private final Main $this;
-                private final Renderer renderer;
-                private boolean isUpPressed;
-                private boolean isDownPressed;
-                private boolean isLeftPressed;
-                private boolean isRightPressed;
-                private boolean isZPressed;
-                private boolean isQPressed;
-                private boolean isSPressed;
-                private boolean isDPressed;
-                private boolean isAPressed;
-                private boolean isEPressed;
 
-                private MyKeyListener(Main m, Renderer r) {
-                        $this = m;
-                        renderer = r;
-                }
+		// Classe interne chargée des évenements claviers
+	private static final class MyKeyListener extends KeyAdapter {
 
-                private void doThat(){
-                        if(isDownPressed)
-                                renderer.alphaX += 0.8;
-                        if(isUpPressed)
-                                renderer.alphaX -= 0.8;
-                        if(isLeftPressed)
-                                renderer.alphaY -= 0.8;
-                        if(isRightPressed)
-                                renderer.alphaY += 0.8;
-                        if(isZPressed)
-                            	renderer.transX += 0.2;
-	                    if(isQPressed)
-	                            renderer.transY -= 0.2;
-	                    if(isSPressed)
-	                            renderer.transX -= 0.2;
-	                    if(isDPressed)
-	                            renderer.transY += 0.2;
-	                    if(isAPressed)
-                            	renderer.transZ -= 0.2;
-	                    if(isEPressed)
-                            	renderer.transZ += 0.2;
-                }
+		private final Main $this;
+		private final Renderer renderer;
+		private boolean isUpPressed;
+		private boolean isDownPressed;
+		private boolean isLeftPressed;
+		private boolean isRightPressed;
+		private boolean isZPressed;
+		private boolean isQPressed;
+		private boolean isSPressed;
+		private boolean isDPressed;
+		private boolean isAPressed;
+		private boolean isEPressed;
 
-                public void keyPressed(KeyEvent e) {
-                        switch (e.getKeyCode()) {
-                                case KeyEvent.VK_ESCAPE:
-                                        $this.animator.stop();
-                                        System.exit(0);
-                                        break;
-                                case KeyEvent.VK_UP:
-                                        isUpPressed = true;
-                                        break;
-                                case KeyEvent.VK_DOWN:
-                                        isDownPressed = true;
-                                        break;
-                                case KeyEvent.VK_RIGHT:
-                                        isRightPressed = true;
-                                        break;
-                                case KeyEvent.VK_LEFT:
-                                        isLeftPressed = true;
-                                        break;
-                                case KeyEvent.VK_Z:
-                                    isZPressed = true;
-                                    break;
-                                case KeyEvent.VK_Q:
-                                    isQPressed = true;
-                                    break;
-                                case KeyEvent.VK_S:
-                                    isSPressed = true;
-                                    break;
-                                case KeyEvent.VK_D:
-                                    isDPressed = true;
-                                    break;
-                                case KeyEvent.VK_A:
-                                    isAPressed = true;
-                                    break;
-                                case KeyEvent.VK_E:
-                                    isEPressed = true;
-                                    break;
-                        }
-                        doThat();
-                }
+		private MyKeyListener(Main m, Renderer r) {
+			$this = m;
+			renderer = r;
+		}
 
-                public void keyReleased(KeyEvent e) {
-                        switch (e.getKeyCode()) {
-                                case KeyEvent.VK_UP:
-                                        isUpPressed = false;
-                                        break;
-                                case KeyEvent.VK_DOWN:
-                                        isDownPressed = false;
-                                        break;
-                                case KeyEvent.VK_RIGHT:
-                                        isRightPressed = false;
-                                        break;
-                                case KeyEvent.VK_LEFT:
-                                        isLeftPressed = false;
-                                        break;
-                                case KeyEvent.VK_Z:
-                                    isZPressed = false;
-                                    break;
-                                case KeyEvent.VK_Q:
-                                    isQPressed = false;
-                                    break;
-                                case KeyEvent.VK_S:
-                                    isSPressed = false;
-                                    break;
-                                case KeyEvent.VK_D:
-                                    isDPressed = false;
-                                    break;
-                                case KeyEvent.VK_A:
-                                    isAPressed = false;
-                                    break;
-                                case KeyEvent.VK_E:
-                                    isEPressed = false;
-                                    break;
-                        }
-                }
-        }
-        
-        
-        public Main(){
-                // Appel du constructeur parent
-                super();
-                // Spécifie la taille de la fenêtre
-                setSize(500,500);
-                // Renseigne le titre de la fenetre
-                setTitle("Manipulation d'une boite en 3D");
-                // Permet de centrer la fenetre à l'ouverture
-                setLocationRelativeTo(null);                          
+		private void doThat(){
+			if(isDownPressed)
+				renderer.alphaX += 0.8;
+			if(isUpPressed)
+				renderer.alphaX -= 0.8;
+			if(isLeftPressed)
+				renderer.alphaY -= 0.8;
+			if(isRightPressed)
+				renderer.alphaY += 0.8;
+			if(isZPressed)
+				renderer.transX += 0.2;
+			if(isQPressed)
+				renderer.transY -= 0.2;
+			if(isSPressed)
+				renderer.transX -= 0.2;
+			if(isDPressed)
+				renderer.transY += 0.2;
+			if(isAPressed)
+				renderer.transZ -= 0.2;
+			if(isEPressed)
+				renderer.transZ += 0.2;
+		}
 
-                // Initialisation du manager d'événements OpenGL
-                Renderer renderer = new Renderer();                  
+		public void keyPressed(KeyEvent e) {
+			switch (e.getKeyCode()) {
+				case KeyEvent.VK_ESCAPE:
+					$this.animator.stop();
+					System.exit(0);
+					break;
+				case KeyEvent.VK_UP:
+					isUpPressed = true;
+					break;
+				case KeyEvent.VK_DOWN:
+					isDownPressed = true;
+					break;
+				case KeyEvent.VK_RIGHT:
+					isRightPressed = true;
+					break;
+				case KeyEvent.VK_LEFT:
+					isLeftPressed = true;
+					break;
+				case KeyEvent.VK_Z:
+					isZPressed = true;
+					break;
+				case KeyEvent.VK_Q:
+					isQPressed = true;
+					break;
+				case KeyEvent.VK_S:
+					isSPressed = true;
+					break;
+				case KeyEvent.VK_D:
+					isDPressed = true;
+					break;
+				case KeyEvent.VK_A:
+					isAPressed = true;
+					break;
+				case KeyEvent.VK_E:
+					isEPressed = true;
+					break;
+			}
+			doThat();
+		}
 
-                // Initialisation du canvas
-                canvas = new GLCanvas();      
-                // Spécification d'un manager d'événements OpenGL
-                canvas.addGLEventListener(renderer);          
+		public void keyReleased(KeyEvent e) {
+			switch (e.getKeyCode()) {
+				case KeyEvent.VK_UP:
+					isUpPressed = false;
+					break;
+				case KeyEvent.VK_DOWN:
+					isDownPressed = false;
+					break;
+				case KeyEvent.VK_RIGHT:
+					isRightPressed = false;
+					break;
+				case KeyEvent.VK_LEFT:
+					isLeftPressed = false;
+					break;
+				case KeyEvent.VK_Z:
+					isZPressed = false;
+					break;
+				case KeyEvent.VK_Q:
+					isQPressed = false;
+					break;
+				case KeyEvent.VK_S:
+					isSPressed = false;
+					break;
+				case KeyEvent.VK_D:
+					isDPressed = false;
+					break;
+				case KeyEvent.VK_A:
+					isAPressed = false;
+					break;
+				case KeyEvent.VK_E:
+					isEPressed = false;
+					break;
+			}
+		}
+	}
 
-                // Ajout du canvas au frame
-                add(canvas);                                                          
 
-                // Initialisation de l'animateur avec ici 50 FPS
-                animator  = new FPSAnimator(canvas, 50);      
+	public Main(){
+		// Appel du constructeur parent
+		super();
+		// Spécifie la taille de la fenêtre
+		setSize(500,500);
+		// Renseigne le titre de la fenetre
+		setTitle("Manipulation d'une boite en 3D");
+		// Permet de centrer la fenetre à l'ouverture
+		setLocationRelativeTo(null);                          
 
-                // Adaptater permettant libérer les ressources lors de la fermeture de la fenêtre
-                addWindowListener(new WindowAdapter() {              
+		// Initialisation du manager d'événements OpenGL
+		Renderer renderer = new Renderer();                  
 
-                @Override
-                public void windowClosing(WindowEvent e) {
-                        // Arret de l'animateur
-                        animator.stop();
-                        // Fermeture de l'application
-                                System.exit(0);                            
-                        }
-                });
-                
-                // Liaison de la fenêtre et du canvas avec le manager d'evenement clavier
-                addKeyListener(new MyKeyListener(this, renderer));    
-        canvas.addKeyListener(new MyKeyListener(this, renderer));
-                
-                // Masque les menus et bordures de la fenetre
-                setUndecorated(true);
-                // Permet d'être en mode plein écran*/
-        setExtendedState(Frame.MAXIMIZED_BOTH);        
+		// Initialisation du canvas
+		canvas = new GLCanvas();      
+		// Spécification d'un manager d'événements OpenGL
+		canvas.addGLEventListener(renderer);          
 
-                // Démarrage de l'animateur
-                animator.start();
-                // Affichage de la fenêtre
-                setVisible(true);                                                    
-        }
+		// Ajout du canvas au frame
+		add(canvas);                                                          
 
-        public static void main(String[] args) {
-                // Les méthodes AWT doivent être invoquées depuis l'EDT d'AWT
-                EventQueue.invokeLater(new Runnable() {              
-                        @Override
-                        public void run() {
-                                new Main();
-                        }
-                });  
-        }
+		// Initialisation de l'animateur avec ici 50 FPS
+		animator = new FPSAnimator(canvas, 50);      
+
+		// Adaptater permettant libérer les ressources lors de la fermeture de la fenêtre
+		addWindowListener(new WindowAdapter() {              
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// Arret de l'animateur
+				animator.stop();
+				// Fermeture de l'application
+				System.exit(0);                            
+			}
+		});
+
+		// Liaison de la fenêtre et du canvas avec le manager d'evenement clavier
+		addKeyListener(new MyKeyListener(this, renderer));    
+		canvas.addKeyListener(new MyKeyListener(this, renderer));
+
+		// Masque les menus et bordures de la fenetre
+		setUndecorated(true);
+		// Permet d'être en mode plein écran*/
+		setExtendedState(Frame.MAXIMIZED_BOTH);        
+
+		// Démarrage de l'animateur
+		animator.start();
+		// Affichage de la fenêtre
+		setVisible(true);                                                    
+	}
+
+	public static void main(String[] args) {
+		// Les méthodes AWT doivent être invoquées depuis l'EDT d'AWT
+		EventQueue.invokeLater(new Runnable() {              
+			@Override
+			public void run() {
+				new Main();
+			}
+		});  
+	}
 
 }
--- src/Renderer.java
+++ src/Renderer.java
@@ -5,125 +5,125 @@ import javax.media.opengl.glu.GLU;
 
 public class Renderer implements GLEventListener {
 
-        // Utilitaire donnant accès aux commandes bas niveau d'OpenGL
-        private GLU glu;
-        // Angle courant de rotation sur l'axe X
-        public float alphaX=0f;        
-        // Angle courant de rotation sur l'axe Y
-        public float alphaY=0f;        
-        // Angle courant de rotation sur l'axe Z
-        public float alphaZ=0f;   
-        // Angle courant de rotation sur l'axe X
-        public float transX=0f;        
-        // Angle courant de rotation sur l'axe Y
-        public float transY=0f;        
-        // Angle courant de rotation sur l'axe Z
-        public float transZ=0f;       
+	// Utilitaire donnant accès aux commandes bas niveau d'OpenGL
+	private GLU glu;
+	// Angle courant de rotation sur l'axe X
+	public float alphaX=0f;        
+	// Angle courant de rotation sur l'axe Y
+	public float alphaY=0f;        
+	// Angle courant de rotation sur l'axe Z
+	public float alphaZ=0f;   
+	// Angle courant de rotation sur l'axe X
+	public float transX=0f;        
+	// Angle courant de rotation sur l'axe Y
+	public float transY=0f;        
+	// Angle courant de rotation sur l'axe Z
+	public float transZ=0f;       
 
-        
-        /**
-        * Appelée immédiatemment lors de l'initialisation du context graphique OpenGl (canvas).
-        * N'est appelée qu'une seule fois.
-        */
-        @Override
-        public void init(GLAutoDrawable drawable) {
-                // Récupération du contexte en GL2
-                GL2 gl = drawable.getGL().getGL2();                                                  
-                // Initialisation de l'utilitaire
-                glu = new GLU();                                                                      
-                // Remplissage du contexte avec du NOIR
-                gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);                                              
-                // Configuration la profondeur au maximum
-                gl.glClearDepth(1.0f);                                                                                        
-                // Autorisation de faire un rendu avec une perspective
-                gl.glEnable(GL2.GL_DEPTH_TEST);                                                              
-                // Restriction de l'affichage aux éléments(Z<=Max)
-                gl.glDepthFunc(GL2.GL_LEQUAL);                                                                
-                // Correction pour la meilleure perspective
-                gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST);        
-                // Joli mélange de couleur, et lissage des textures
-                gl.glShadeModel(GL2.GL_SMOOTH);                                                              
-        }
-                
-                
-        /**
-        * Appelée par l'animateur pour executer le rendu graphique.
-        */
-        @Override
-        public void display(GLAutoDrawable drawable) {
-                // Récupération du contexte en GL2
-                GL2 gl = drawable.getGL().getGL2();                                                  
-                // Réinitialisation de la scène (effacement des tampons)
-                gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);        
-                // Reinitialisation de la matrice courante
-                gl.glLoadIdentity();                                                                                  
+	
+	/**
+	* Appelée immédiatemment lors de l'initialisation du context graphique OpenGl (canvas).
+	* N'est appelée qu'une seule fois.
+	*/
+	@Override
+	public void init(GLAutoDrawable drawable) {
+		// Récupération du contexte en GL2
+		GL2 gl = drawable.getGL().getGL2();                                                  
+		// Initialisation de l'utilitaire
+		glu = new GLU();                                                                      
+		// Remplissage du contexte avec du NOIR
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);                                              
+		// Configuration la profondeur au maximum
+		gl.glClearDepth(1.0f);                                                                                        
+		// Autorisation de faire un rendu avec une perspective
+		gl.glEnable(GL2.GL_DEPTH_TEST);                                                              
+		// Restriction de l'affichage aux éléments(Z<=Max)
+		gl.glDepthFunc(GL2.GL_LEQUAL);                                                                
+		// Correction pour la meilleure perspective
+		gl.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST);        
+		// Joli mélange de couleur, et lissage des textures
+		gl.glShadeModel(GL2.GL_SMOOTH);                                                              
+	}
+		
+		
+	/**
+	* Appelée par l'animateur pour executer le rendu graphique.
+	*/
+	@Override
+	public void display(GLAutoDrawable drawable) {
+		// Récupération du contexte en GL2
+		GL2 gl = drawable.getGL().getGL2();                                                  
+		// Réinitialisation de la scène (effacement des tampons)
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);        
+		// Reinitialisation de la matrice courante
+		gl.glLoadIdentity();                                                                                  
 
-                /* (Alt aux translations) Placement de la caméra au point (4,0,12)
-                Direction vers l'origine de la scène (0,0,0)
-                Inclinaison nulle car la vue suit l'axe vertical (y) */
-                glu.gluLookAt(        4f, 0f, 12f,
-                                                0f, 0f, 0f,  
-                                                0f, 1f, 0f  
-                );
+		/* (Alt aux translations) Placement de la caméra au point (4,0,12)
+		Direction vers l'origine de la scène (0,0,0)
+		Inclinaison nulle car la vue suit l'axe vertical (y) */
+		glu.gluLookAt(  4f, 0f, 12f,
+						0f, 0f, 0f,  
+						0f, 1f, 0f  
+		);
 
-                // Rotation de la matrice courante de l'angle alpha autour de l'axe x (1,0,0)
-                gl.glRotatef(alphaX,        
-                                        1f, 0f, 0f
-                );
+		// Rotation de la matrice courante de l'angle alpha autour de l'axe x (1,0,0)
+		gl.glRotatef(alphaX,        
+					1f, 0f, 0f
+		);
 
-                // Rotation de la matrice courante de l'angle alpha autour de l'axe x (0,1,0)
-                gl.glRotatef(alphaY,
-                                        0f, 1f, 0f
-                );
+		// Rotation de la matrice courante de l'angle alpha autour de l'axe x (0,1,0)
+		gl.glRotatef(alphaY,
+					0f, 1f, 0f
+		);
 
-                // Rotation de la matrice courante de l'angle alpha autour de l'axe x (0,0,1)
-                gl.glRotatef(alphaZ,
-                                        0f, 0f, 1f 
-                );
-                
-                // Translation sur les axes x, y, z
-                gl.glTranslatef(transX, transY, transZ);
+		// Rotation de la matrice courante de l'angle alpha autour de l'axe x (0,0,1)
+		gl.glRotatef(alphaZ,
+					0f, 0f, 1f 
+		);
+		
+		// Translation sur les axes x, y, z
+		gl.glTranslatef(transX, transY, transZ);
 
-                // Tous les dessins utltérieurs subiront la transformation : Dessin d'un cube
-                new Cube(2.0f, 0, 0, 0).draw(gl);                                                            
-        }
+		// Tous les dessins utltérieurs subiront la transformation : Dessin d'un cube
+		new Cube(2.0f, 0, 0, 0).draw(gl);                                                            
+	}
 
-        
-        /**
-        * Appelée avant la destruction du context OpenGL. Permet de liberer les ressources telles que les buffers.
-        */
-        @Override
-        public void dispose(GLAutoDrawable arg0) {
-                // Non utilisée ici mais sa déclaration est obligatoire de par l'interface
-        }
+	
+	/**
+	* Appelée avant la destruction du context OpenGL. Permet de liberer les ressources telles que les buffers.
+	*/
+	@Override
+	public void dispose(GLAutoDrawable arg0) {
+		// Non utilisée ici mais sa déclaration est obligatoire de par l'interface
+	}
 
-        /**
-        * Appelée lorsque la fenetre d'affichage est redimensionnée,
-        * Mais aussi lorsque la fenetre est affichée pour la première fois
-        */
-        @Override
-        public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
-                // Récupération du contexte en GL2
-                GL2 gl = drawable.getGL().getGL2();
+	/**
+	* Appelée lorsque la fenetre d'affichage est redimensionnée,
+	* Mais aussi lorsque la fenetre est affichée pour la première fois
+	*/
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+		// Récupération du contexte en GL2
+		GL2 gl = drawable.getGL().getGL2();
 
-                // Alternative à la division par zéro
-                if (height == 0) height = 1;                                                                          
+		// Alternative à la division par zéro
+		if (height == 0) height = 1;                                                                          
 
-                // Configuration de la zone d'affichage OpenGL
-                gl.glViewport(0, 0, width, height);                                                          
+		// Configuration de la zone d'affichage OpenGL
+		gl.glViewport(0, 0, width, height);                                                          
 
-                // Spécification d'une matrice de projection en perspective
-                // Passage en mode définition de la PROJECTION
-                gl.glMatrixMode(GL2.GL_PROJECTION);  
-                // Reinitialisation de la matrice courante
-                gl.glLoadIdentity();
-                // Angle d'observation en degrés, echelle entre largeur et hauteur, intervalle de projection
-                glu.gluPerspective(45.0, (float)width/height, 0.1, 100.0);
+		// Spécification d'une matrice de projection en perspective
+		// Passage en mode définition de la PROJECTION
+		gl.glMatrixMode(GL2.GL_PROJECTION);  
+		// Reinitialisation de la matrice courante
+		gl.glLoadIdentity();
+		// Angle d'observation en degrés, echelle entre largeur et hauteur, intervalle de projection
+		glu.gluPerspective(45.0, (float)width/height, 0.1, 100.0);
 
-                // Selection de la transformation point de vue
-                gl.glMatrixMode(GL2.GL_MODELVIEW);
-                // Reinitialisation de la matrice courante
-                gl.glLoadIdentity();
-        }
+		// Selection de la transformation point de vue
+		gl.glMatrixMode(GL2.GL_MODELVIEW);
+		// Reinitialisation de la matrice courante
+		gl.glLoadIdentity();
+	}
 
 }
